66d4a3d6dff85b9a77b27f1c3129ba3fd925317c
Tharsikan <thar.navaratnam@gmail.com>
2022-01-10 21:49:51 +0000

    add test for correct error messages

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe User, type: :model do
         password:"12345678",
         password_confirmation:"12345678"
       )
+
       expect(@user.valid?).to be_truthy
     end
 
@@ -23,7 +24,9 @@ RSpec.describe User, type: :model do
         password: "12345678",
         password_confirmation: "123456789"
       )
+
       expect(@user.valid?).to be_falsy
+      expect(@user.errors.full_messages[0]).to eq("Password confirmation doesn't match Password")
     end
 
     it "should NOT create the user if password is not present" do
@@ -34,7 +37,9 @@ RSpec.describe User, type: :model do
         password: nil,
         password_confirmation: "123456789"
       )
+
       expect(@user.valid?).to be_falsy
+      expect(@user.errors.full_messages[0]).to eq("Password can't be blank")
     end
 
     it "should NOT create the user if password_confirmation is not present" do
@@ -45,7 +50,9 @@ RSpec.describe User, type: :model do
         password: "12345678",
         password_confirmation: nil
       )
+
       expect(@user.valid?).to be_falsy
+      expect(@user.errors.full_messages[0]).to eq("Password confirmation can't be blank")
     end
 
     it "should NOT create the user if the email already exists" do
@@ -57,6 +64,7 @@ RSpec.describe User, type: :model do
         password_confirmation: "12345678"
       )
       @user.save
+
       @user2 = User.new(
         first_name: "John",
         last_name: "Cena",
@@ -64,7 +72,9 @@ RSpec.describe User, type: :model do
         password: "12345678abc",
         password_confirmation: "12345678abc"
       )
+
       expect(@user2.valid?).to be_falsy
+      expect(@user2.errors.full_messages[0]).to eq("Email has already been taken")
     end
 
     it "should NOT create the user if first_name is not present" do
@@ -75,7 +85,9 @@ RSpec.describe User, type: :model do
         password: "123456789",
         password_confirmation: "123456789"
       )
+
       expect(@user.valid?).to be_falsy
+      expect(@user.errors.full_messages[0]).to eq("First name can't be blank")
     end
 
     it "should NOT create the user if last_name is not present" do
@@ -87,6 +99,7 @@ RSpec.describe User, type: :model do
         password_confirmation: "123456789"
       )
       expect(@user.valid?).to be_falsy
+      expect(@user.errors.full_messages[0]).to eq("Last name can't be blank")
     end
 
     it "should NOT create the user if email is not present" do
@@ -98,6 +111,7 @@ RSpec.describe User, type: :model do
         password_confirmation: "123456789"
       )
       expect(@user.valid?).to be_falsy
+      expect(@user.errors.full_messages[0]).to eq("Email can't be blank")
     end
 
 
