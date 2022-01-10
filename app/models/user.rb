af282cb28556e7b27ed1c10c11a17f46fc293462
Tharsikan <thar.navaratnam@gmail.com>
2022-01-10 22:38:17 +0000

    remove white space when finding user by email

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }
 
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email)
+    user = User.find_by_email(email.strip)
     return user && user.authenticate(password) ? user : nil
   end
 
