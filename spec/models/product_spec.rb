bd813f00dec8b28f69af0adc80e2d4447d830e81
Tharsikan <thar.navaratnam@gmail.com>
2022-01-10 20:59:44 +0000

    test for correct error message for each case

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,33 +3,37 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
 
-    before(:all) do
+    before(:each) do
       @category = Category.new name: "Food"
     end
 
-    it "should be valid if the product has a name, price, quantity, and category" do
-      @product = Product.new name: "Apple", price: 3, quantity: 100, category: @category
+    it "should be valid if the product has a name, price_cents, quantity, and category" do
+      @product = Product.new name: "Apple", price_cents: 3, quantity: 100, category: @category
       expect(@product.valid?).to be_truthy
     end
 
     it "should not be valid if name is not present" do
-      @product = Product.new  name: nil, price: 3, quantity: 100, category: @category
+      @product = Product.new  name: nil, price_cents: 3, quantity: 100, category: @category
       expect(@product.valid?).to be_falsy
+      expect(@product.errors.full_messages[0]).to eq("Name can't be blank")
     end
 
-    it "should not be valid if price is not present" do
-      @product = Product.new name: "Apple", price: nil, quantity: 100, category: @category
+    it "should not be valid if price_cents is not present" do
+      @product = Product.new name: "Apple", price_cents: nil, quantity: 100, category: @category
       expect(@product.valid?).to be_falsy
+      expect(@product.errors.full_messages[0]).to eq("Price cents is not a number")
     end
 
     it "should not be valid if quantity is not present" do
-      @product = Product.new name: "Apple", price: 3, quantity: nil, category: @category
+      @product = Product.new name: "Apple", price_cents: 3, quantity: nil, category: @category
       expect(@product.valid?).to be_falsy
+      expect(@product.errors.full_messages[0]).to eq("Quantity can't be blank")
     end
 
     it "should not be valid if category is not present" do
-      @product = Product.new name: "Apple", price: 3, quantity: 100, category: nil
+      @product = Product.new name: "Apple", price_cents: 3, quantity: 100, category: nil
       expect(@product.valid?).to be_falsy
+      expect(@product.errors.full_messages[0]).to eq("Category can't be blank")
     end
 
 
