46050d549e8cf867b4ea05450bb084f7d737607e
Tharsikan <thar.navaratnam@gmail.com>
2022-01-10 20:16:38 +0000

    refactor using before(:all)

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,26 +3,26 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
 
-    it "should be valid if the product has a name, price, quantity, and category" do
+    before(:all) do
       @category = Category.new name: "Food"
+    end
+
+    it "should be valid if the product has a name, price, quantity, and category" do
       @product = Product.new name: "Apple", price: 3, quantity: 100, category: @category
       expect(@product.valid?).to be_truthy
     end
 
     it "should not be valid if name is not present" do
-      @category = Category.new name: "Food"
       @product = Product.new  name: nil, price: 3, quantity: 100, category: @category
       expect(@product.valid?).to be_falsy
     end
 
     it "should not be valid if price is not present" do
-      @category = Category.new name: "Food"
       @product = Product.new name: "Apple", price: nil, quantity: 100, category: @category
       expect(@product.valid?).to be_falsy
     end
 
     it "should not be valid if quantity is not present" do
-      @category = Category.new name: "Food"
       @product = Product.new name: "Apple", price: 3, quantity: nil, category: @category
       expect(@product.valid?).to be_falsy
     end
