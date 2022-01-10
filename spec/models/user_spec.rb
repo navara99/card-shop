44dcf63078a0a8b409a3900a5ab8ece39fb4e137
Tharsikan <thar.navaratnam@gmail.com>
2022-01-10 21:19:10 +0000

    add test for when password and confirmation dont match

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,12 +12,19 @@ RSpec.describe User, type: :model do
         password:"12345678",
         password_confirmation:"12345678"
       )
-
       expect(@user.valid?).to be_truthy
-
     end
 
-
+    it "should NOT create the user if password and password_confirmation do not match" do
+      @user = User.new(
+        first_name: "Jesus",
+        last_name: "Barnum",
+        email: "JesusDBarnum@dayrep.com",
+        password:"12345678",
+        password_confirmation:"123456789"
+      )
+      expect(@user.valid?).to be_falsy
+    end
 
 
 
