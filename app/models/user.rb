f923ba58d28b09599c17a889e0ae6f50377534ea
Tharsikan <thar.navaratnam@gmail.com>
2022-01-10 21:34:48 +0000

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+  validates :first_name, presence:true
+  validates :last_name, presence:true
+  validates :password, presence:true, length: { minimum: 8 }
+  validates :password_confirmation, presence:true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
