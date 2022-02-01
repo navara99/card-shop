ed5ee2ae9c1bbcd6bf9007b0eab9434b43db4689
Tharsikan <thar.navaratnam@gmail.com>
2022-02-01 05:05:33 +0000

    update seeds with cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,9 +25,14 @@ end
 
 puts "Finding or Creating Categories ..."
 
-cat1 = Category.find_or_create_by! name: 'Apparel'
-cat2 = Category.find_or_create_by! name: 'Electronics'
-cat3 = Category.find_or_create_by! name: 'Furniture'
+# cat1 = Category.find_or_create_by! name: 'Apparel'
+# cat2 = Category.find_or_create_by! name: 'Electronics'
+# cat3 = Category.find_or_create_by! name: 'Furniture'
+cat1 = Category.find_or_create_by! name: 'Pokemon'
+cat2 = Category.find_or_create_by! name: 'Yu-Gi-Oh'
+cat3 = Category.find_or_create_by! name: 'Magic The Gathering'
+cat4 = Category.find_or_create_by! name: 'Basketball'
+cat5 = Category.find_or_create_by! name: 'Baseball'
 
 ## PRODUCTS
 
@@ -36,101 +41,163 @@ puts "Re-creating Products ..."
 Product.destroy_all
 
 cat1.products.create!({
-  name:  'Men\'s Classy shirt',
+  name:  'Alakazam - 1/102 - Holo Rare - Shadowless Edition',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel1.jpg'),
+  image: open_asset('alakazam.jpg'),
   quantity: 10,
-  price: 64.99
-})
+  price: 60.99
+});
 
 cat1.products.create!({
-  name:  'Women\'s Zebra pants',
+  name:  'Poliwrath - 13/102 - Holo Rare - 1st Edition',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel2.jpg'),
-  quantity: 18,
-  price: 124.99
-})
+  image: open_asset('poliwrath.jpg'),
+  quantity: 7,
+  price: 400.99
+});
 
 cat1.products.create!({
-  name:  'Hipster Hat',
+  name:  'Machamp - 8/102 - Holo Rare - 1st Edition - SHADOWLESS Edition',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel3.jpg'),
-  quantity: 4,
-  price: 34.49
-})
+  image: open_asset('machamp.jpg'),
+  quantity: 5,
+  price: 30.99
+});
 
-cat1.products.create!({
-  name:  'Hipster Socks',
+cat5.products.create!({
+  name:  '2018 Topps Big League Baseball Hobby Box',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel4.jpg'),
-  quantity: 8,
-  price: 25.00
-})
-
-cat1.products.create!({
-  name:  'Russian Spy Shoes',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel5.jpg'),
-  quantity: 8,
-  price: 1_225.00
-})
-
-cat1.products.create!({
-  name:  'Human Feet Shoes',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel6.jpg'),
-  quantity: 82,
-  price: 224.50
-})
-
+  image: open_asset('big-league-box.jpg'),
+  quantity: 2,
+  price: 29.99
+});
 
 cat2.products.create!({
-  name:  'Modern Skateboards',
+  name:  'Rocket Warrior - CT2-EN005 - Secret Rare - Limited Edition',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics1.jpg'),
-  quantity: 40,
-  price: 164.49
-})
+  image: open_asset('rocketwarrior.jpg'),
+  quantity: 12,
+  price: 3.99
+});
 
 cat2.products.create!({
-  name:  'Hotdog Slicer',
+  name:  'Cyber Dragon - CT03-EN002 - Secret Rare - Limited Edition',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics2.jpg'),
-  quantity: 3,
-  price: 26.00
-})
+  image: open_asset('cyberdragon.jpg'),
+  quantity: 12,
+  price: 5.49
+});
 
 cat2.products.create!({
-  name:  'World\'s Largest Smartwatch',
+  name:  'Elemental Hero Shining Flare Wingman - CT03-EN004 - Secret Rare - Limited Edition',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics3.jpg'),
-  quantity: 32,
-  price: 2_026.29
-})
-
-cat3.products.create!({
-  name:  'Optimal Sleeping Bed',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture1.jpg'),
-  quantity: 320,
-  price: 3_052.00
-})
-
-cat3.products.create!({
-  name:  'Electric Chair',
-  description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture2.jpg'),
+  image: open_asset('elementalhero.jpg'),
   quantity: 0,
-  price: 987.65
-})
+  price: 5.99
+});
+
+cat2.products.create!({
+  name:  'Rainbow Dragon - CT04-EN005 - Secret Rare - Limited Edition',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('rainbowdragon.jpg'),
+  quantity: 12,
+  price: 8.99
+});
+
+cat1.products.create!({
+  name:  'Dragonair - 18/102 - Rare - 1st Edition',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('dragonair.jpg'),
+  quantity: 2,
+  price: 500.99
+});
 
 cat3.products.create!({
-  name:  'Red Bookshelf',
+  name:  'Liliana, Dreadhorde General - Promo Pack',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture3.jpg'),
-  quantity: 23,
-  price: 2_483.75
-})
+  image: open_asset('liliana.jpg'),
+  quantity: 9,
+  price: 29.99
+});
 
+cat3.products.create!({
+  name:  'Teferi, Time Raveler',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('teferi.png'),
+  quantity: 4,
+  price: 21.99
+});
+
+cat4.products.create!({
+  name:  '2015-16 Panini Prestige NBA Basketball HANGER Box',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('prestige.jpg'),
+  quantity: 8,
+  price: 64.99
+});
+
+cat4.products.create!({
+  name:  '2021-22 Panini HOOPS NBA Basketball Hobby Box',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('21-hobby-box.jpg'),
+  quantity: 3,
+  price: 209.99
+});
+
+cat3.products.create!({
+  name:  'Finale of Devastation',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('finale.png'),
+  quantity: 0,
+  price: 39.99
+});
+
+cat4.products.create!({
+  name:  '2021-22 Panini HOOPS NBA Basketball Hobby Box',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('17-hobby-box.jpg'),
+  quantity: 2,
+  price: 219.99
+});
+
+cat1.products.create!({
+  name:  'Magikarp - 35/102 - Uncommon - Shadowless Edition',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('magikarp.jpg'),
+  quantity: 0,
+  price: 10.99
+});
+
+cat2.products.create!({
+  name:  'Blue-Eyes Alternative Ultimate Dragon - TN19-EN001 - Prismatic Secret Rare - Limited Edition',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('blueeyes.png'),
+  quantity: 3,
+  price: 5.49
+});
+
+cat5.products.create!({
+  name:  '2021 Panini Mosaic CHOICE Baseball Hobby Box',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('21base.jpg'),
+  quantity: 3,
+  price: 89.99
+});
+
+cat1.products.create!({
+  name:  'Mewtwo - 10/102 - Holo Rare - Shadowless Edition',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('mewtwo.jpg'),
+  quantity: 1,
+  price: 95.99
+});
+
+cat2.products.create!({
+  name:  'Dark Magician - BPT-007 - Secret Rare',
+  description: Faker::Hipster.paragraph(4),
+  image: open_asset('darkmagician.jpg'),
+  quantity: 12,
+  price: 27.99
+});
 
 puts "DONE!"
